f1edd3245f4cc1a19c12da4d47ed978f5fc66801
ikshvaku <61447188+ikshvaku-codes@users.noreply.github.com>
2023-12-10 12:20:56 +0530

    Day 1

---
+++ src/Day 1.py
@@ -0,0 +1,14 @@
+#Commenting
+#This is very simple stuff but the idea of this exercise is to revise and review the coding practice from the very basic.
+#Day 1:- Printing, Commenting, Debugging, String Manupilation and Variables.
+
+name = input("What is your name?")#Creating Variables and taking input.
+age = input("What is your age?")
+hometown = input("What is your hometown?")
+highest_study = input("What is your highest study?")
+highest_school = input("Which school you went for highest study?")
+hobbies = input("What are your hobbies?")
+#String Manupilation
+output = f"My name is {name}. I hail from {hometown}. I am {age} years old. I went to {highest_school} for {highest_study}. I like to {hobbies}."
+#Printing the output.
+print(output)
